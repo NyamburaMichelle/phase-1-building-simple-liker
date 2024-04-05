5a5f7b053a38a89255a000b0a0851ad3bc0fc2f4
Michelle <nyamburamichelle400@gmail.com>
2024-04-05 20:42:42 +0300

    tries

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const like=document.querySelector(".like") 
+function clickHeart(e) {
+  const heart=e.target;
+  mimicServerCall()
+  .then(function(response) {
+    alert("You notified the server!");
+    alert(response);
+    like.innerText= FULL_HEART;
+})
 
+.catch(function(error) {
+ alert("Something went wrong")
+})
+
+}
+for(const char of like.innerHTML) {
+ like.addEventListener("click", clickHeart);
+
+}
+console.log(clickHeart);
 
 
 
@@ -23,3 +42,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
